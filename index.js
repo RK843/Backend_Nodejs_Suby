9a52f8c13b01e804e6344acfa7266102afcb7905
RK843 <114291974+RK843@users.noreply.github.com>
2025-03-01 19:10:36 +0530

    added path module

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const productRoutes = require('./routes/productRoute');
 const cors = require('cors');
 const path = require('path');
 
-const app = express();
+const app = express()
 
 const PORT = process.env.PORT || 4000;
 
